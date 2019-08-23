52d571da30ef0dd77d03a4d2aeeb0d351fda0291
Kara Odle <karajo.odle@gmail.com>
2019-08-23 22:32:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,6 +2,17 @@ def find_element_index(array, value_to_find)
   
   array.include?(value_to_find) ? array.index(value_to_find) : nil
   
+# length = array.length 
+  
+#   length.times do |index|
+    
+#     if array[index] == value_to_find
+#       return index
+#     end 
+#   end 
+  
+#   return nil 
+
 end
 
 def find_max_value(array)
