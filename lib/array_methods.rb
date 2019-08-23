7023092dea42de551dba8ed0ef8eca0d5bd1a94d
Kara Odle <karajo.odle@gmail.com>
2019-08-23 22:31:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,6 @@
 def find_element_index(array, value_to_find)
   
-  length = array.length 
   
-  length.times do |index|
-    
-    if array[index] == value_to_find
-      return index
-    end 
-  end 
-  
-  return nil 
   
 end
 
